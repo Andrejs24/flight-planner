35269c3d7baac8af158a41c96ea12817c462bca0
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-07-25 13:35:05 +0300

    Updated AdminFlightController ->(all ResponseEntity<Flight> are replaced with Flight and update all ErrorHandling

--- src/main/java/io/codelex/flightplanner/flight/api/admin/responses/AdminFlightController.java
+++ src/main/java/io/codelex/flightplanner/flight/api/admin/responses/AdminFlightController.java
@@ -5,12 +5,9 @@ import io.codelex.flightplanner.flight.domain.Flight;
 import io.codelex.flightplanner.flight.exeptions.DuplicateFlightException;
 import io.codelex.flightplanner.flight.request.CreateFlightRequest;
 import io.codelex.flightplanner.flight.response.FlightService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.server.ResponseStatusException;
 import java.time.DateTimeException;
 
 
@@ -24,49 +21,42 @@ public class AdminFlightController {
         this.flightService = flightService;
     }
 
-    Logger logger = LoggerFactory.getLogger(AdminFlightController.class);
-
 
     @PutMapping("/flights")
-    public ResponseEntity<Flight> addFlight(@RequestBody CreateFlightRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public Flight addFlight(@RequestBody CreateFlightRequest request) {
 
         try {
-            Flight addedFlight = flightService.createFlight(request);
-            logger.info("Flight created successfully: " + addedFlight.getId());
-            return ResponseEntity.status(HttpStatus.CREATED).body(addedFlight);
+            return flightService.createFlight(request);
         } catch (DuplicateFlightException e) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Flight already exists", e);
         } catch (NullPointerException e) {
-            logger.info("This is a problem");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This is a problem", e);
         } catch (DateTimeException e) {
-            logger.info("Or this is a problem");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid date format", e);
         }
 
     }
 
     @DeleteMapping("flights/{id}")
-    public ResponseEntity<Flight> deleteFlightById(@PathVariable long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFlightById(@PathVariable long id) {
         try {
             flightService.deleteFlightById(id);
-            logger.info("Flight is successfully deleted");
-            return ResponseEntity.status(HttpStatus.OK).build();
         } catch (Exception e) {
-            logger.info("No such flight registered");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No such flight registered", e);
         }
 
     }
 
     @GetMapping("flights/{id}")
-    public ResponseEntity<Flight> searchFlightById(@PathVariable long id) {
+    @ResponseStatus(HttpStatus.OK)
+    public Flight searchFlightById(@PathVariable long id) {
         try {
-            Flight flightFounded = flightService.searchFlightById(id);
-            return ResponseEntity.status(HttpStatus.OK).body(flightFounded);
+            return flightService.searchFlightById(id);
         } catch (Exception e) {
-            logger.info("No such flight registered");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No such flight registered", e);
+
         }
     }
 
