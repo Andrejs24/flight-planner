9f30540ab2ea4ac6ee4b416cdcfb344ac6790e93
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-07-27 11:44:47 +0300

    Added anotations

--- src/main/java/io/codelex/flightplanner/FlightPlannerApplication.java
+++ src/main/java/io/codelex/flightplanner/FlightPlannerApplication.java
@@ -2,8 +2,10 @@ package io.codelex.flightplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.annotation.Validated;
 
 @SpringBootApplication
+@Validated
 public class FlightPlannerApplication {
 
 	public static void main(String[] args) {
