421d0f3181dadfe947d81b7057c8647bab5566a6
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-07-27 11:47:11 +0300

    Replaces generic with String and added annotations

--- src/main/java/io/codelex/flightplanner/flight/request/SearchFlightRequest.java
+++ src/main/java/io/codelex/flightplanner/flight/request/SearchFlightRequest.java
@@ -1,34 +1,43 @@
 package io.codelex.flightplanner.flight.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 
-public class SearchFlightRequest <T>{
+public class SearchFlightRequest {
 
-    private T from;
-
-    private T to;
-
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    private String from;
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    private String to;
+    @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-DD")
     private String departureDate;
 
 
     public SearchFlightRequest() {
+
     }
 
-    public T getFrom() {
+    public String getFrom() {
         return from;
     }
 
-    public void setFrom(T from) {
+    public void setFrom(String from) {
         this.from = from;
     }
 
-    public T getTo() {
+    public String getTo() {
         return to;
     }
 
-    public void setTo(T to) {
+    public void setTo(String to) {
         this.to = to;
     }
 
@@ -40,4 +49,6 @@ public class SearchFlightRequest <T>{
         this.departureDate = departureDate;
     }
 }
+
+
 
