acf4d18a1b3a7f5c73da13e45a78c46544ae944a
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-08-05 14:47:01 +0300

    Created @Query for isAirportExist and searchAirportByPhrase methods

--- src/main/java/io/codelex/flightplanner/flight/response/AirportDataBaseRepository.java
+++ src/main/java/io/codelex/flightplanner/flight/response/AirportDataBaseRepository.java
@@ -2,7 +2,10 @@ package io.codelex.flightplanner.flight.response;
 
 import io.codelex.flightplanner.flight.domain.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AirportDataBaseRepository extends JpaRepository<Airport, Long> {
@@ -11,4 +14,17 @@ public interface AirportDataBaseRepository extends JpaRepository<Airport, Long> 
 
     Airport findByAirport(String airport);
 
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM Airport a " +
+            "WHERE a.airport = :airport " +
+            "AND a.city = :city " +
+            "AND a.country = :country ")
+    boolean isAirportExist(@Param("country") String country,
+                           @Param("city") String city,
+                           @Param("airport") String airport);
+
+    @Query("SELECT a FROM Airport a " +
+            "WHERE LOWER(a.airport) LIKE %:phrase% " +
+            "OR LOWER(a.city) LIKE %:phrase% " +
+            "OR LOWER(a.country) LIKE %:phrase%")
+    List<Airport> searchAirportsByPhrase(@Param("phrase") String phrase);
 }
