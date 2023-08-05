0d6a2a2b1bf600e9ab5359ff8f344f97695efba4
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-08-05 14:47:27 +0300

    Created @Query for searchFlights method

--- src/main/java/io/codelex/flightplanner/flight/response/FlightDataBaseRepository.java
+++ src/main/java/io/codelex/flightplanner/flight/response/FlightDataBaseRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface FlightDataBaseRepository extends JpaRepository<Flight, Long> {
@@ -27,5 +29,14 @@ public interface FlightDataBaseRepository extends JpaRepository<Flight, Long> {
                           @Param("toAirport") String toAirport,
                           @Param("carrier") String carrier);
 
-//@Query("select ")
+    @Query("select f FROM Flight f where f.from.city = :from " +
+            "and f.to.city = :to " +
+            "and f.departureTime = :departureDate")
+    List<Flight> searchFlights(@Param("from") String from,
+                               @Param("to") String to,
+                               @Param("departureDate") LocalDateTime departureDate);
+
+
 }
+
+
